d038fd440f7a48abdfc8c43737e6db1020ae98aa
SO114514 <159113637+SO114514@users.noreply.github.com>
2024-04-22 02:07:32 +0200

    Business.cs

---
+++ Business.cs
@@ -0,0 +1,16 @@
+namespace WebAppTest.Models
+{
+    [Table("Business")]
+    public class Business
+    {
+        public int Id { get; set; }
+        public string Company { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string JobTitle { get; set; }
+        public string email { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Country { get; set; }
+        public string Address { get; set; }
+    }
+}
