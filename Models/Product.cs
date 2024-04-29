05a8012813c6cfc5f22b2f142243063c81449536
SO114514 <159113637+SO114514@users.noreply.github.com>
2024-04-29 21:06:10 +0200

    Product.cs

---
+++ Models/Product.cs
@@ -0,0 +1,12 @@
+namespace SeiAzureWebApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Items { get; set; }
+        public int Quantity { get; set; }
+        public int price { get; set; }
+        public string Category { get; set; }
+        public string ArticleNo { get; set; }
+    }
+}
